418452b68569637e122bc693fc51be9ef12fd978
Erik Barajas <erik.barajas93@gmail.com>
2016-08-24 18:23:27 -0500

    Initial version of Login with Django

---
+++ envLogin/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ErikBarajas/Documents/Programming/Python/Login_Django/envLogin/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
